9371dcda7228ebebb92796f547212d5430943375
holyhost <2024212728@qq.com>
2024-04-03 21:13:22 +0800

    init

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import { Welcome } from '../components/Welcome/Welcome';
 import { ColorSchemeToggle } from '../components/ColorSchemeToggle/ColorSchemeToggle';
+import classes from './app.module.css'
 
 export default function HomePage() {
   return (
     <>
-      <Welcome />
-      <ColorSchemeToggle />
+      <div className={classes.mainbg}>
+        123
+      </div>
     </>
   );
 }
