379bf5a0a0e0010f454d545c6f26a993abcd7a41
holyhost <2024212728@qq.com>
2024-04-18 20:49:02 +0800

    add nick name

--- components/WishResult/WishResult.tsx
+++ components/WishResult/WishResult.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
 import classes from './WishResult.module.css'
 import { IconStarFilled } from '@tabler/icons-react'
 import { Character } from '@/constants/characters'
@@ -8,20 +8,30 @@ type Props = {
 }
 
 const WishResult = ({friend}: Props) => {
+  const [ready, setReady] = useState(false)
+  const eleAudio = useRef<HTMLAudioElement>(null)
+  const playResult = ()=>{
+    eleAudio.current?.play()
+    setReady(true)
+  }
   return (
     <div className={classes.container}>
-      <audio src='/assets/mp3/wished.wav' autoPlay/>
+      <audio ref={eleAudio} onCanPlayThrough={playResult} src='/assets/mp3/wished.wav'/>
       <img className={classes.cover} src={`/assets/images/character/${friend?.nick || friend.id}.png`}/>
-      <div className={classes.info}>
-        <img src={`/assets/images/${friend.type}.png`} alt="character element" />
-        <div className={classes.nameContainer}>
-          {friend.name}
-          <p style={{lineHeight: '2rem', margin:'0', marginTop: '-4px'}}>
-            {new Array(friend.star).fill(1).map((i,j) => <IconStarFilled key={i+j} className={classes.star} color='gold'/>)}
-        </p>
+      {ready ? (<>
+        <div className={classes.info}>
+          <img src={`/assets/images/${friend.type}.png`} alt="character element" />
+          <div className={classes.nameContainer}>
+            {friend.name}
+            <p style={{lineHeight: '2rem', margin:'0', marginTop: '-4px'}}>
+              {new Array(friend.star).fill(1).map((i,j) => <IconStarFilled key={i+j} className={classes.star} color='gold'/>)}
+          </p>
+          </div>
         </div>
-        
-      </div>
+        <div className={classes.nickName + ' ' + classes.threeD}>
+          {'孙悟空'.split('').map(i => <span className={classes.star} key={i}>{i}
+            </span>)}
+            </div> </>) : null}
     </div>
   )
 }
