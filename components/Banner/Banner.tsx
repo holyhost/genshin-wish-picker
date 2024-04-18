a733573ea3db737404c27432c2ccc071308c0258
holyhost <2024212728@qq.com>
2024-04-18 22:06:03 +0800

    screen compability

--- components/Banner/Banner.tsx
+++ components/Banner/Banner.tsx
@@ -18,88 +18,88 @@ type Props = {
   doWish: (count: number, position: number) => void
 }
 
-const Banner = ({curIndex=0, posts, doWish}: Props) => {
+const Banner = ({ curIndex = 0, posts, doWish }: Props) => {
   const [curPosition, setCurPosition] = useState(curIndex)
   const [embla, setEmbla] = useState<Embla | null>(null);
   const updateBanner = (index: number) => {
     embla?.scrollTo(index)
   }
-  
+
   return (
     <Stack
-          justify="space-between"
-          gap="sm"
-          className={classes.container}
-        >
-      <Group justify='space-between' style={{alignItems: 'flex-start'}}>
-      <Text c={'white'} fw={600} mt={'14px'} w={'256px'}>
-        <img
-          style={{ height: '50px', width: '50px', marginRight: '2rem', paddingBottom: '6px' }} 
-          alt='wish star' src='/assets/icons/star.png' 
-          className='icon' />
-        祈愿
-      </Text>
-      <div>
+      justify="space-between"
+      gap="sm"
+      className={classes.container}
+    >
+      <Group justify='space-between' style={{ alignItems: 'flex-start' }}>
+        <Text c={'white'} fw={600} mt={'14px'} className={classes.title}>
+          <img
+            alt='wish star' src='/assets/icons/star.png'
+            className={'icon ' + classes.titleIcon} />
+          祈愿
+        </Text>
+        <div>
           {posts.map(
-            (post, index) => 
+            (post, index) =>
               <img
+                className={classes.indexBanner}
                 loading='lazy'
                 key={'img-' + post.id}
-                onClick={()=> updateBanner(index)}
-                src={`/assets/images/${post.indexUrl}${index === curPosition ? '-selected' : ''}.png`} 
+                onClick={() => updateBanner(index)}
+                src={`/assets/images/${post.indexUrl}${index === curPosition ? '-selected' : ''}.png`}
                 alt="wish pool index" />
           )}
-      </div>
-      <Center mt={'26px'}>
-        <div className={classes.stone}>
-          <img src='/assets/icons/stone.png' className='icon' alt='Genshin stone' />
-          <span style={{ marginLeft: '.5rem', marginRight: '.5rem' }}>19998</span>
-          <IconCirclePlus color='#e3e3e3' />
         </div>
-        <div className={classes.stone}>
-          <img src='/assets/icons/pink-ball.png' className='icon' alt='Genshin stone' />
-          <span style={{ marginLeft: '.5rem', marginRight: '.5rem' }}>990</span>
-        </div>
-        <img src='/assets/icons/closing-button.png' className={classes.closeBtn} alt='Genshin stone' />
-      </Center>
+        <Center mt={'26px'}>
+          <div className={classes.stone}>
+            <img src='/assets/icons/stone.png' className='icon' alt='Genshin stone' />
+            <span style={{ marginLeft: '.5rem', marginRight: '.5rem' }}>19998</span>
+            <IconCirclePlus color='#e3e3e3' />
+          </div>
+          <div className={classes.stone}>
+            <img src='/assets/icons/pink-ball.png' className='icon' alt='Genshin stone' />
+            <span style={{ marginLeft: '.5rem', marginRight: '.5rem' }}>990</span>
+          </div>
+          <img src='/assets/icons/closing-button.png' className={classes.closeBtn} alt='Genshin stone' />
+        </Center>
 
-    </Group>
+      </Group>
       <Carousel
         orientation='horizontal'
         loop
-        styles={{control: {background: 'transparent', border: 'none'}}}
-        previousControlIcon={<NavButton name='left'/>}
-        nextControlIcon={<NavButton name='right'/>}
+        styles={{ control: { background: 'transparent', border: 'none' } }}
+        previousControlIcon={<NavButton name='left' />}
+        nextControlIcon={<NavButton name='right' />}
         getEmblaApi={setEmbla}
         onSlideChange={setCurPosition}>
         {posts.map(post => (
           <Carousel.Slide key={post.id}>
             <Group justify='center'>
-              <img loading='lazy' className={classes.post} alt='Genshi impact wish' src={'/assets/images/' + post.indexUrl+'-up.png'}/>
+              <img loading='lazy' className={classes.post} alt='Genshi impact wish' src={'/assets/images/' + post.indexUrl + '-up.png'} />
             </Group>
           </Carousel.Slide>
         ))}
-        </Carousel>
-        {/* bottom layout */}
-        <Group justify='space-between'>
-            <div style={{marginTop: '-22px'}}>
-              <Text c={'white'}>
-                <img className='icon' src='/assets/icons/star-light.png' alt='icon star light'/> 999
-                <img style={{marginLeft: '2rem'}} className='icon' src='/assets/icons/star-dust.png' alt='icon star dust'/> 1999
-              </Text>
-              <Group mt={'4px'}>
-                <TextButton title='尘辉兑换'/>
-                <TextButton title='详情'/>
-                <TextButton title='历史记录'/>
-            </Group>
-            </div>
-            
-            <Group>
-              <WishButton  doWish={() => doWish(1, curPosition)}/>
-              <WishButton count={10} doWish={() => doWish(10, curPosition)}/>
-            </Group>
-            
+      </Carousel>
+      {/* bottom layout */}
+      <Group justify='space-between'>
+        <div style={{ marginTop: '-22px' }}>
+          <Text c={'white'}>
+            <img className='icon' src='/assets/icons/star-light.png' alt='icon star light' /> 999
+            <img style={{ marginLeft: '2rem' }} className='icon' src='/assets/icons/star-dust.png' alt='icon star dust' /> 1999
+          </Text>
+          <Group mt={'4px'}>
+            <TextButton title='尘辉兑换' />
+            <TextButton title='详情' />
+            <TextButton title='历史记录' />
           </Group>
+        </div>
+
+        <Group>
+          <WishButton doWish={() => doWish(1, curPosition)} />
+          <WishButton count={10} doWish={() => doWish(10, curPosition)} />
+        </Group>
+
+      </Group>
     </Stack>
   )
 }
