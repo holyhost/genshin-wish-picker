92930b469b8103e43ef5a86566e67545ad5eb752
holyhost <2024212728@qq.com>
2024-04-20 22:50:10 +0800

    add result for 10 wishes

--- components/WishResult/WishResult.tsx
+++ components/WishResult/WishResult.tsx
@@ -10,6 +10,7 @@ type Props = {
 
 const WishResult = ({count=1, friends}: Props) => {
   const [ready, setReady] = useState(false)
+  const [showRoles, setShowRoles] = useState(false)
   const [friend, setFriend] = useState(friends[0])
   const [leftCount, setLeftCount] = useState(count-1)
   const eleAudio = useRef<HTMLAudioElement>(null)
@@ -22,14 +23,29 @@ const WishResult = ({count=1, friends}: Props) => {
       eleAudio.current?.play()
       setFriend(friends[friends.length - leftCount])
       setLeftCount(leftCount-1)
+    }else if(count > 1){
+      setShowRoles(true)
     }
     
   }
   return (
     <div className={classes.container} onClick={goNext}>
       <audio ref={eleAudio} onCanPlayThrough={playResult} src='/assets/mp3/wished.wav'/>
-      <img className={classes.cover} src={`/assets/images/character/${friend.id}.png`}/>
-      {ready ? (<>
+      {showRoles ? <div className={classes.roles}>
+            {friends.map((f, ind) => (
+              <div className={classes.card} key={f.id + "-role" + ind} style={{backgroundImage: 'url(/assets/images/bg-role-item.png)'}}>
+                <img className={classes.roleItem} src={`/assets/images/character/${f.id}.png`} alt="role result" />
+                {f.nick && <div className={classes.roleNickContainer}>
+                  {f.nick.split('').map(w => <span key={w} className={classes.roleNick + ' ' + classes.star + ' ' + classes.threeD}>{w}</span>)}
+                </div>}
+                <div className={classes.extra}>
+                  <img src="/assets/icons/extra-star-5.png" alt="extra" />
+                  <img style={{marginBottom: '16px'}} src="/assets/icons/extra-5.png" alt="extra" />
+                </div>
+              </div>
+            ))}
+      </div> : <img className={classes.cover} src={`/assets/images/character/${friend.id}.png`}/>}
+      {ready && !showRoles ? (<>
         <div className={classes.info}>
           <img src={`/assets/images/${friend.type}.png`} alt="character element" />
           <div className={classes.nameContainer}>
@@ -44,7 +60,8 @@ const WishResult = ({count=1, friends}: Props) => {
             </span>)}
         </div> }
         </>) : null}
-      {leftCount? <img className={classes.gonext} src="/assets/icons/right.png" alt="show next result" /> : null}
+      {/* {leftCount? <img className={classes.gonext} src="/assets/icons/right.png" alt="show next result" /> : null} */}
+      
     </div>
   )
 }
