c305a3dd56907c3d84f1f364622023fe77175b94
holyhost <2024212728@qq.com>
2024-04-20 16:40:41 +0800

    update assets for characters

--- constants/characters.ts
+++ constants/characters.ts
@@ -180,7 +180,7 @@ export const Characters: Character[] = [
     {
         name: '纳维莱特',
         type: Ele.Water,
-        nick: '',
+        nick: '孙悟空',
         id: 'naweilaite',
         star: 5
     },
@@ -190,5 +190,145 @@ export const Characters: Character[] = [
         nick: '',
         id: 'wangye',
         star: 5
-    }
+    },
+    {
+        name: '七七',
+        type: Ele.Ice,
+        nick: '',
+        id: '77',
+        star: 5
+    },
+    {
+        name: '白术',
+        type: Ele.Dendro,
+        nick: '',
+        id: 'baishu',
+        star: 5
+    },
+    {
+        name: '魈',
+        type: Ele.Wind,
+        nick: '',
+        id: 'xiao',
+        star: 5
+    },
+    {
+        name: '北斗',
+        type: Ele.Electric,
+        nick: '',
+        id: 'beidou',
+        star: 4
+    },
+    {
+        name: '凝光',
+        type: Ele.Geo,
+        nick: '',
+        id: 'ningguang',
+        star: 4
+    },
+    {
+        name: '香菱',
+        type: Ele.Fire,
+        nick: '',
+        id: 'xiangling',
+        star: 4
+    },
+    {
+        name: '行秋',
+        type: Ele.Water,
+        nick: '',
+        id: 'xingqiu',
+        star: 4
+    },
+    {
+        name: '重云',
+        type: Ele.Ice,
+        nick: '',
+        id: 'chongyun',
+        star: 4
+    },
+    {
+        name: '刻晴',
+        type: Ele.Electric,
+        nick: '',
+        id: 'keqing',
+        star: 5
+    },
+    {
+        name: '达达利亚',
+        type: Ele.Water,
+        nick: '',
+        id: 'dadaliya',
+        star: 5
+    },
+    {
+        name: '钟离',
+        type: Ele.Geo,
+        nick: '',
+        id: 'zhongli',
+        star: 5
+    },
+    {
+        name: '辛焱',
+        type: Ele.Fire,
+        nick: '',
+        id: 'xinyan',
+        star: 4
+    },
+    {
+        name: '胡桃',
+        type: Ele.Fire,
+        nick: '',
+        id: 'hutao',
+        star: 5
+    },
+    {
+        name: '烟绯',
+        type: Ele.Fire,
+        nick: '',
+        id: 'yanfei',
+        star: 4
+    },
+    {
+        name: '申鹤',
+        type: Ele.Ice,
+        nick: '',
+        id: 'shenhe',
+        star: 5
+    },
+    {
+        name: '云堇',
+        type: Ele.Geo,
+        nick: '',
+        id: 'yunjin',
+        star: 4
+    },
+    {
+        name: '夜兰',
+        type: Ele.Water,
+        nick: '',
+        id: 'yelan',
+        star: 5
+    },
+    {
+        name: '瑶瑶',
+        type: Ele.Dendro,
+        nick: '',
+        id: 'yaoyao',
+        star: 4
+    },
+    {
+        name: '闲云',
+        type: Ele.Wind,
+        nick: '',
+        id: 'xianyun',
+        star: 5
+    },
+    {
+        name: '嘉明',
+        type: Ele.Fire,
+        nick: '',
+        id: 'jiaming',
+        star: 4
+    },
 ]
