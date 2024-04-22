977577a9d6bd070d6cd753d9c3002867b19b4bc7
holyhost <2024212728@qq.com>
2024-04-22 22:03:48 +0800

    update button

--- components/Button/GenshinButton/GenshinButton.tsx
+++ components/Button/GenshinButton/GenshinButton.tsx
@@ -1,21 +1,21 @@
 import React from 'react'
-import classes from './RetryButton.module.css'
+import classes from './GenshinButton.module.css'
 
 type Props = {
   title: string,
-  size?: string,
+  type?: 'ok' | 'cancel' | 'try',
   click?: ()=> void
 }
 
-const RetryButton = ({title, size='36px', click}: Props) => {
+const GenshinButton = ({title, type='ok', click}: Props) => {
   return (
     <div onClick={click} className={classes.container} style={{width: title.length * 16 + 100 + 'px'}}>
       <img
-        src='/assets/icons/try.png'
+        src={`/assets/icons/${type}.png`}
         alt='retry button'/>
       <span>{title}</span>
     </div>
   )
 }
 
-export default RetryButton
+export default GenshinButton
